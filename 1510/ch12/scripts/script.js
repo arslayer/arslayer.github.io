f4f6aee61e35223da94cea5cd755c8fb15fd1057
Davin A <33433413+arslayer@users.noreply.github.com>
2023-11-29 22:44:28 -0500

    add Ch12 lab

---
+++ 1510/ch12/scripts/script.js
@@ -0,0 +1,10 @@
+/*
+    Student Name: Davin Asiala
+    File Name: script.js
+    Date: 11/29/2023
+*/
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function(){
+    $('.hero').height($(window).height());
+});
